bf93a74199e1adc6b6159c552756fbbd4d2bba19
jason-edwards <jedwards@live.com.au>
2020-03-30 20:49:28 +1100

    Program.cs updated with simple check of Finnhub Client

--- InvestmentSimulator/Program.cs
+++ InvestmentSimulator/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using ThreeFourteen.Finnhub.Client;
 
 namespace InvestmentSimulator
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = CreateConfiguration();
+
+            string apiKey = File.ReadAllText(config["finnhubkeypath"]);
+            
+            var client = new FinnhubClient(apiKey);
+            string symbol = "AAPL";
+            var company = await client.Stock.GetCompany(symbol);
+            
+
+            Console.WriteLine($"Company: {symbol},{Environment.NewLine}Description: {company.Description}");
+        }
+
+        static IConfigurationRoot CreateConfiguration()
+        {
+            string path = Path.GetFullPath("../../../../");
+            Console.WriteLine(path);
+
+            return new ConfigurationBuilder()
+                //.AddInMemoryCollection()
+                //.AddEnvironmentVariables()
+                .SetBasePath(path)
+                .AddJsonFile("settings.json", false)
+                //.AddCommandLine(args)
+                .Build();
         }
     }
 }
