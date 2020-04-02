41f26e3c01e8194ca3c0605b110e6145f72b10ad
jason-edwards <jedwards@live.com.au>
2020-04-02 11:26:44 +1100

    Added serilog to db

--- InvestmentSimulator/Program.cs
+++ InvestmentSimulator/Program.cs
@@ -2,7 +2,9 @@
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 using ThreeFourteen.Finnhub.Client;
+
 
 namespace InvestmentSimulator
 {
@@ -11,21 +13,39 @@ namespace InvestmentSimulator
         static async Task Main(string[] args)
         {
             var config = CreateConfiguration();
+            OpenLogger(config);
+            
+            Log.Information("This informational message will be written to SQLite database");
+            
 
             string apiKey = File.ReadAllText(config["finnhubkeypath"]);
-            
             var client = new FinnhubClient(apiKey);
+            
+
             string symbol = "AAPL";
             var company = await client.Stock.GetCompany(symbol);
             
 
             Console.WriteLine($"Company: {symbol},{Environment.NewLine}Description: {company.Description}");
+             
+            CloseLogger();
+        }
+
+        static ILogger OpenLogger(IConfiguration config)
+        {
+            return Log.Logger = new LoggerConfiguration()
+                .WriteTo.SQLite(config["logdbpath"])
+                .CreateLogger();
+        }
+
+        static void CloseLogger()
+        {
+            Log.CloseAndFlush();
         }
 
         static IConfigurationRoot CreateConfiguration()
         {
             string path = Path.GetFullPath("../../../../");
-            Console.WriteLine(path);
 
             return new ConfigurationBuilder()
                 //.AddInMemoryCollection()
@@ -35,5 +55,7 @@ namespace InvestmentSimulator
                 //.AddCommandLine(args)
                 .Build();
         }
+
+        
     }
 }
